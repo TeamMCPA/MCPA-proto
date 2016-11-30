bd1eeeaa4a6124e133f195b171c79d6cf1ba7569
Jeremy <chengyu06242013@hotmail.com>
2016-11-30 15:14:58 -0500

    Add files via upload

--- main.m
+++ main.m
@@ -20,11 +20,6 @@
 % assignment.
 %
 
-
-
-
-
-
 %Hardcoding parameters:
 
 % For testing, please un-comment the data_file_name_cell variable and enter
@@ -32,47 +27,57 @@
 
 %data_file_name_cell = {'file1.nirs' 'file2.nirs' 'file3.nirs' 'file4.nirs'};
 
-data_file_name_cell = data_file_name_cell';
-time_window = 0:100;
-condition1 = 1;
-condition2 = 2;
-incl_channels = [3:5,13:19];
+% data_file_name_cell = data_file_name_cell';
+% time_window = 0:100;
+% condition1 = 1;
+% condition2 = 2;
+% incl_channels = [3:5,13:19];
 %incl_subjects = [1 3:6 8:14 17:18 20:21, 23:25];
-incl_subjects = [1 2 3];
+% incl_subjects = [1 2 3];
 
-
-
-MCPA_output = output_MCPA_struct(data_file_name_cell, incl_subjects, incl_channels, time_window);
-
-window_averages_struct = summarize_MCPA_Struct(@nanmean, MCPA_output);
+% MCPA_output = output_MCPA_struct(data_file_name_cell, incl_subjects, incl_channels, time_window);
+% 
+% window_averages_struct = summarize_MCPA_Struct(@nanmean, MCPA_output);
 
 % infant-level decoding
-WDIT_infantleveldecoding = leave_one_Ss_out_classifyAverages(window_averages_struct,condition1,condition2);
+% WDIT_infantleveldecoding = leave_one_Ss_out_classifyAverages(window_averages_struct,condition1,condition2);
 % trial-level coding
-WDIT_trialleveldecoding = leave_one_Ss_out_classifyIndividualEvents(window_averages_struct,condition1,condition2);
-
-
-
+% WDIT_trialleveldecoding = leave_one_Ss_out_classifyIndividualEvents(window_averages_struct,condition1,condition2);
 
 
 % examining infant-level decoding across different subset sizes for
 % channels
-setsize = 5;  
-WDIT_infantleveldecoding_setsize5 = leave_one_Ss_out_classifyAverages(window_averages_struct,condition1,condition2,setsize);
-WDIT_trialleveldecoding_setsize5 = leave_one_Ss_out_classifyIndividualEvents(window_averages_struct,condition1,condition2,setsize);
+% setsize = 5;  
+% WDIT_infantleveldecoding_setsize5 = leave_one_Ss_out_classifyAverages(window_averages_struct,condition1,condition2,setsize);
+% WDIT_trialleveldecoding_setsize5 = leave_one_Ss_out_classifyIndividualEvents(window_averages_struct,condition1,condition2,setsize);
 
 
 % now restrict the analyses to just the top 3 channels that were most
 % informative and we have almost perfect performance for the 
-incl_channels = [3:5,13:19];
-incl_channels = incl_channels([1,3,8]);
+% incl_channels = [3:5,13:19];
+% incl_channels = incl_channels([1,3,8]);
 
 
-MCPA_output = output_MCPA_struct(data_file_name_cell, incl_subjects, incl_channels, time_window);
-window_averages_struct = summarize_MCPA_Struct(@nanmean, MCPA_output);
+% MCPA_output = output_MCPA_struct(data_file_name_cell, incl_subjects, incl_channels, time_window);
+% window_averages_struct = summarize_MCPA_Struct(@nanmean, MCPA_output);
+% 
+% WDIT_infantleveldecoding_top2 = leave_one_Ss_out_classifyAverages(window_averages_struct,condition1,condition2);
+% WDIT_trialleveldecoding_top3 = leave_one_Ss_out_classifyIndividualEvents(window_averages_struct,condition1,condition2);
 
-WDIT_infantleveldecoding_top2 = leave_one_Ss_out_classifyAverages(window_averages_struct,condition1,condition2);
-WDIT_trialleveldecoding_top3 = leave_one_Ss_out_classifyIndividualEvents(window_averages_struct,condition1,condition2);
+raw_nirs_file_name1 = 'Test_for_Joszef_001-RNAv3_MES_Probe1.nirs';
+MCP_struct = create_MCP_struct(raw_nirs_file_name1);
+
+raw_nirs_file_name2 = 'Test_for_Joszef_002-BDZ_MES_Probe1.nirs';
+MCP_struct_2 = create_MCP_struct(raw_nirs_file_name2);
+
+raw_nirs_file_name3 = 'Test_for_Joszef_003-SZ_MES_Probe1.nirs';
+MCP_struct_3 = create_MCP_struct(raw_nirs_file_name3);
+
+
+MCP_struct = [MCP_struct, MCP_struct_2, MCP_struct_3];
+
+
+
 
 
 
